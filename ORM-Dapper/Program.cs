de9a7307239b104a6260302d1bcdfcaf4ab9ff56
Levi Williams <ljw758@hotmail.com>
2023-07-09 18:22:56 -0500

    Completed Exercise 1 and 2

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+
+            #region Department Section
+
+            var departmentRepo = new DapperDepartmentRepository(conn);
+
+            var departments = departmentRepo.GetAll();
+
+            departmentRepo.InsertDepartment("Levi's New Department");
+
+            foreach(var department in departments)
+            {
+                Console.WriteLine(department.DepartmentID);
+                Console.WriteLine(department.Name);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            #endregion
+
+            var productRepository = new DapperProductRepository(conn);
+
+            var products = productRepository.GetAll();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.ProductID);
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.CategoryID);
+                Console.WriteLine(product.OnSale);
+                Console.WriteLine(product.StockLevel);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
         }
     }
+
+    
 }
